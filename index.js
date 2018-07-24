684a2ea2bde58f885949cbd27dff6a85187bbeb5
Nathaniel Gibbons <ngibbons333@gmail.com>
2018-07-24 23:07:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  
+  lString == string.toUpperCase();
+  
+  if (string === 'I love you, Grandma.'){
+    console.log('I love you, too.');
+  }
+  
+  if (string === lString) {
+    console.log('YES INDEED!');
+    }
+  else {
+    console.log('I can"t hear you!');
+  }
+}
